090f70fbbb9a46120080c250aa2f42eba44f3b94
Rafael Queiroz Santos <rafaelqsantos33@gmail.com>
2023-12-04 11:54:27 -0300

    Att 04-12

--- Estrutura de Dados I/Trabalho 03/DynamicListLib.h
+++ Estrutura de Dados I/Trabalho 03/DynamicListLib.h
@@ -29,7 +29,7 @@ int chkListaVazia(T_Lista *lista) {
 }
 
 // Inserção no fundo
-void append(T_Lista *lista, T_Item item) {
+void inserir_no_fim(T_Lista *lista, T_Item item) {
     T_No *novoNo = (T_No*)malloc(sizeof(T_No));
     novoNo->item = item;
     novoNo->proximo = NULL;
@@ -60,16 +60,136 @@ void inserir_na_frente(T_Lista *lista, T_Item item) {
     lista->qtdeAtual++;
 }
 
-void imprimir_lista(T_Lista *lista) {
+// Remoção do fim
+void remover_do_fim(T_Lista *lista) {
     T_No *atual = lista->primeiro;
 
-    printf("Lista: ");
+    // Caso lista vazia: Exibe a mensagem de alerta e não efetua nenhum operação
+    if (chkListaVazia(lista)) {
+        printf("Lista vazia !! Nada a remover...\n");
+        return;
+    }
+
+    // Caso Lista unitária: Lista com apenas um elemento
+    if (lista->primeiro->proximo == NULL) {
+        free(lista->primeiro);
+        lista->primeiro = NULL;
+        lista->ultimo = NULL;
+        lista->qtdeAtual--;
+        return;
+    }
+
+    // Caso geral: Percorre a lista toda verificando o proximo do proximo
+    while (atual->proximo->proximo != NULL) {
+        atual = atual->proximo;
+    }
+
+    free(atual->proximo);
+    atual->proximo = NULL;
+    lista->ultimo = atual;
+    lista->qtdeAtual--;
+}
+
+void remover_do_inicio(T_Lista *lista) {
+
+    // Caso lista vazia: Exibe a mensagem de alerta e não efetua nenhum operação
+    if(chkListaVazia(lista)) {
+        printf("\nLista vazia!! Nada a remover...\n");
+        return;
+    }
+
+    // Caso Lista unitária: Lista com apenas um elemento
+    if (lista->primeiro->proximo == NULL) {
+        free(lista->primeiro);
+        lista->primeiro = NULL;
+        lista->ultimo = NULL;
+        lista->qtdeAtual--;
+        return;
+    }
+
+    // Caso geral:
+    T_No *auxiliar = lista->primeiro;
+    free(lista->primeiro);
+    lista->primeiro = auxiliar->proximo;
+    lista->qtdeAtual--;
+}
+
+void remover_pelo_indice(T_Lista *lista, int indice) {
+    // Verificando o indice
+    if (indice < 0 || indice >= lista->qtdeAtual) {
+        printf("\nIndice invalido!! Nada a remover...\n");
+        return;
+    }
+
+    // Caso especial: removendo o primeiro elemento
+    if (indice == 0) {
+        remover_do_inicio(lista);
+        return;
+    }
+
+    // Caso Geral: Percorre até o nó anterior
+    T_No *anterior = lista->primeiro;
+    for (int i = 0;i < indice-1;i++) anterior = anterior->proximo;
+
+    T_No *removido = anterior->proximo;
+
+    // Atualiza a referencia pulando o no a ser removido
+    anterior->proximo = removido->proximo;
+
+    if(removido == lista->ultimo) lista->ultimo = anterior;
+
+    free(removido);
+
+    lista->qtdeAtual--;
+
+}
+
+void inserir_pelo_indice(T_Lista *lista,T_Item item, int indice) {
+    // Verificação do indice
+    if (indice < 0 || indice > lista->qtdeAtual + 1) {
+        printf("Indice invalido!! Nada a inserir...\n");
+        return;
+    }
+
+    // Caso especial: Inserção na primeiro posição
+    if (indice == 0) {
+        inserir_na_frente(lista,item);
+        return;
+    }
+
+    T_No *noNovo = (T_No*)malloc(sizeof(T_No));
+    if (noNovo == NULL) {
+        fprintf(stderr, "Erro ao alocar memoria para o novo no.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    T_No *anterior = lista -> primeiro;
+    noNovo -> item = item;
+
+    for (int i=0;i< indice - 1;i++) anterior = anterior->proximo;
+
+    noNovo->proximo = anterior->proximo;
+    anterior->proximo = noNovo;
+
+    lista->qtdeAtual++;
+}
+
+void imprimir_lista(T_Lista *lista) {
+    if (chkListaVazia(lista)) {
+        printf("\nA LISTA ESTA VAZIA!!!\n\n");
+        return;
+    }
+
+    T_No *atual = lista->primeiro;
+
+    printf("\nLista: ");
 
     while (atual != NULL) {
         printf("> %d ", atual->item.dado);
         atual = atual->proximo;
     }
 
-    printf("\n");
+    printf("\n\n");
 }
+
 
--- Estrutura de Dados I/Trabalho 03/main.cpp
+++ Estrutura de Dados I/Trabalho 03/main.cpp
@@ -2,20 +2,92 @@
 #include <stdlib.h>
 #include "DynamicListLib.h"
 
+int menu();
+
 int main() {
     T_Lista lista;
     T_Item item;
-
     iniciar_lista(&lista);
-    item.dado = 10;
-    append(&lista,item);
-    item.dado = 20;
-    append(&lista,item);
-    item.dado = 30;
-    append(&lista,item);
-    item.dado = 40;
-    inserir_na_frente(&lista,item);
-    imprimir_lista(&lista);
 
+    int resposta_menu = menu();
+    while(resposta_menu != 8) {
+    switch(resposta_menu){
+        case 1:{
+            printf("INSIRA O DADO:\n");
+            scanf("%d",&item.dado);
+            inserir_na_frente(&lista,item);
+            break;
+        }
+        case 2:{
+            printf("INSIRA O DADO:\n");
+            scanf("%d",&item.dado);
+            inserir_no_fim(&lista,item);
+            break;
+        }
+        case 3: {
+            int indice = 0;
+            printf("INSIRA O INDICE:\n");
+            scanf("%d",&indice);
+            printf("INSIRA O DADO:\n");
+            scanf("%d",&item.dado);
+            inserir_pelo_indice(&lista,item,indice);
+            break;
+        }
+        case 4:{
+            printf("REMOVENDO...\n");
+            remover_do_inicio(&lista);
+            printf("REMOVIDO...\n");
+            system("pause");
+            break;
+        }
+        case 5: {
+            printf("REMOVENDO...\n");
+            remover_do_fim(&lista);
+            printf("REMOVIDO...\n");
+            system("pause");
+            break;
+        }
+        case 6: {
+            int indice = 0;
+            printf("INSIRA O INDICE:\n");
+            scanf("%d",&indice);
+            printf("REMOVENDO...\n");
+            remover_pelo_indice(&lista,indice);
+            printf("REMOVIDO...\n");
+            system("pause");
+            break;
+        }
+        case 7:{
+            imprimir_lista(&lista);
+            system("pause");
+            break;
+        }
+        }
+        resposta_menu = menu();
+    }
     return 0;
+}
+
+int menu() {
+    int op;
+    while(1){
+    system("cls");
+    printf("\tSELECIONE O QUE DESEJA FAZER:\n");
+    printf("1 - INSERIR NO INICIO\n");
+    printf("2 - INSERIR NO FIM\n");
+    printf("3 - INSERIR PELO INDICE\n");
+    printf("4 - REMOVER NO INICIO\n");
+    printf("5 - REMOVER NO FIM\n");
+    printf("6 - REMOVER PELO INDICE\n");
+    printf("7 - IMPRIMIR LISTA\n");
+    printf("8 - FECHAR O PROGRAMA\n");
+    scanf("%d",&op);
+    if(op>0 && op<=8) {
+        system("cls");
+        return op;
+    }
+    printf("OPCAO INVALIDA!!! SELCIONE UMA ACEITAVEL\n");
+    system("pause");
+    system("cls");
+    }
 }
